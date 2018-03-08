b172ef52bdb41047a40404667b984a29aa6f9846
Michal Ceresnik <michal.ceresnik@gmail.com>
2018-03-08 21:59:28 +0100

    Added AccountIntegrationTests, added Moq tests.

---
+++ CodingameProject/Tests/ObjectOrientedTraining/AccountIntegrationTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using CodingameProject.Source.ObjectOrientedTraining;
+using NUnit.Framework;
+
+namespace CodingameProject.Tests.ObjectOrientedTraining
+{
+    [TestFixture]
+    public class AccountIntegrationTests
+    {
+        private Account sut;
+        private bool unfreezingWasTriggered;
+
+        private void DoUnfreezeAction(string message)
+        {
+            unfreezingWasTriggered = true;
+            Console.WriteLine(message);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            sut = new Account(DoUnfreezeAction);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            unfreezingWasTriggered = false;
+            sut = null;
+        }
+
+        [Test]
+        public void Id1_DepositToClosedAccount_NoChange()
+        {
+            sut.Deposit(20);
+            sut.Close();
+            sut.Deposit(30);
+            Assert.That(sut.Balance, Is.EqualTo(20));
+        }
+        [Test]
+        public void Id2_Deposit()
+        {
+            sut.Deposit(10);
+            sut.Deposit(1);
+            Assert.That(sut.Balance, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void Id3_WithdrawFromNotVerifiedAccount_NoChange()
+        {
+            sut.Deposit(20);
+            sut.Withdraw(20);
+            Assert.That(sut.Balance, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Id4_WithdrawFromClosedAccount_NoChange()
+        {
+            sut.Deposit(20);
+            sut.Verify();
+            sut.Close();
+            sut.Withdraw(20);
+            Assert.That(sut.Balance, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Id5_Withdraw()
+        {
+            sut.Deposit(10);
+            sut.Verify();
+            sut.Withdraw(1);
+            Assert.That(sut.Balance, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Id8_DepositToNotFrozenAccount_DoesNotTriggerAction()
+        {
+            sut.Deposit(10);
+            Assert.That(unfreezingWasTriggered, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Id10_WithdrawFromFreezedAccount_Withdrawn()
+        {
+            sut.Deposit(30);
+            sut.Verify();
+            sut.Freeze();
+            sut.Withdraw(20);
+            Assert.That(sut.Balance, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Id11_WithdrawFromNotFrozenAccount_DoesNotTriggerAction()
+        {
+            sut.Deposit(10);
+            sut.Verify();
+            sut.Withdraw(1);
+            Assert.That(unfreezingWasTriggered, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Test_DepositToFrozenAccount_TriggersAction()
+        {
+            sut.Verify();
+            sut.Freeze();
+            sut.Deposit(10);
+            Assert.That(unfreezingWasTriggered, Is.EqualTo(true));
+        }
+    }
+}
---
+++ CodingameProject/Tests/ObjectOrientedTraining/AccountUnitTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using CodingameProject.Source.ObjectOrientedTraining;
+using Moq;
+using NUnit.Framework;
+
+namespace CodingameProject.Tests.ObjectOrientedTraining
+{
+    class AccountUnitTests
+    {
+        private Account sut;
+        private Mock<IAccountState> mState;
+
+        private void DoUnfreezeAction(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            mState = new Mock<IAccountState>();
+            sut = new Account(DoUnfreezeAction, mState.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            sut = null;
+        }
+
+        [Test]
+        public void Test_Deposit_DepositOnStateIsCalled()
+        {
+            sut.Deposit(10);
+            mState.Verify(x => x.Deposit(It.IsAny<Action>()), Times.Once);
+        }
+
+        [Test]
+        public void Test_Withdraw_WithdrawOnStateIsCalled()
+        {
+            sut.Withdraw(10);
+            mState.Verify(x => x.Withdraw(It.IsAny<Action>()), Times.Once);
+        }
+
+        [Test]
+        public void Test_Freeze_FreezeOnStateIsCalled()
+        {
+            sut.Freeze();
+            mState.Verify(x => x.Freeze(), Times.Once);
+        }
+
+        [Test]
+        public void Test_Close_CloseOnStateIsCalled()
+        {
+            sut.Close();
+            mState.Verify(x => x.Close(), Times.Once);
+        }
+
+        [Test]
+        public void Test_Verify_VerifyOnStateIsCalled()
+        {
+            sut.Verify();
+            mState.Verify(x => x.Verify(), Times.Once);
+        }
+    }
+}
