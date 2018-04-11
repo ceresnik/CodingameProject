2f8c76713d2f890da1c137d37cfa8bbc165f66c1
Michal Ceresnik <michal.ceresnik@gmail.com>
2018-04-11 23:12:25 +0200

    AsciiArt test added.

--- KGood/Tests/AsciiArtTests.cs
+++ KGood/Tests/AsciiArtTests.cs
@@ -1,4 +1,7 @@
-﻿using KGood.Source;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KGood.Source;
 using NUnit.Framework;
 
 namespace KGood.Tests
@@ -6,13 +9,26 @@ namespace KGood.Tests
     [TestFixture]
     public class AsciiArtTests
     {
-[Test]
+        [Test]
         public void TestAsciiArt()
         {
             string[] inputRows = { ".---.", "|---|", ".---." };
             var result = new AsciiArt().InvertASCIIArt(inputRows);
 
             Assert.That(result, Is.EqualTo(".|.\r\n---\r\n---\r\n---\r\n.|.\r\n"));
+        }
+
+        [Test]
+        public void Test_IndexOfLetterInAlphabet()
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string input = "DAbE";
+            IList<int> indexes = input.Select(c => alphabet.IndexOf(Char.ToLower(c))).ToList();
+            CollectionAssert.Contains(indexes, 3);
+            CollectionAssert.Contains(indexes, 0);
+            CollectionAssert.Contains(indexes, 1);
+            CollectionAssert.Contains(indexes, 4);
+            Assert.That(indexes.Count, Is.EqualTo(4));
         }
     }
 }
