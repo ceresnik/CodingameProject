0233be9997649c85b0314238c18b7dbaa4a9035a
Michal Ceresnik <michal.ceresnik@gmail.com>
2018-07-16 12:38:19 +0200

    ArithmeticSeriesCounter introduced

---
+++ CodingameProject/Source/MathematicalCorner/ArithmeticSeriesCounter.cs
@@ -0,0 +1,27 @@
+﻿namespace CodingameProject.Source.MathematicalCorner
+{
+    /// <summary>
+    /// Arithmetic series is sum of all members of Arithmetic progression.
+    /// </summary>
+    public class ArithmeticSeriesCounter
+    {
+        private readonly int commonDifference;
+
+        public ArithmeticSeriesCounter(int commonDifference)
+        {
+            this.commonDifference = commonDifference;
+        }
+
+        internal int CountSum(int initialItem, int countOfItems)
+        {
+            var lastItem = GetItemByPosition(initialItem, countOfItems);
+            return countOfItems * (initialItem + lastItem) / 2;
+        }
+
+        private int GetItemByPosition(int initialItem, int position)
+        {
+            int itemAtPosition = initialItem + (position - 1) * commonDifference;
+            return itemAtPosition;
+        }
+    }
+}
---
+++ CodingameProject/Tests/MathematicalCorner/ArithmeticSeriesCounterTests.cs
@@ -0,0 +1,31 @@
+﻿using CodingameProject.Source.MathematicalCorner;
+using NUnit.Framework;
+
+namespace CodingameProject.Tests.MathematicalCorner
+{
+    [TestFixture]
+    public class ArithmeticSeriesCounterTests
+    {
+        [TestCase(3, 3, 5, 45)]
+        [TestCase(10, 5, 7, 175)]
+        [TestCase(2, 3, 50, 3775)]
+        [Test]
+        public void Test_PositiveDifference(int initialItem, int commonDifference, int countOfItems, int expectedResult)
+        {
+            var sut = new ArithmeticSeriesCounter(commonDifference);
+            int result = sut.CountSum(initialItem, countOfItems);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase(10, -3, 3, 21)]
+        [TestCase(10, -3, 6, 15)]
+        [TestCase(-10, -3, 6, -105)]
+        [Test]
+        public void Test_NegativeDifference(int initialItem, int commonDifference, int countOfItems, int expectedResult)
+        {
+            var sut = new ArithmeticSeriesCounter(commonDifference);
+            int result = sut.CountSum(initialItem, countOfItems);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
