3e4f1d8ed3b3235e2511d2803ce1071a67a00838
Michal <michal.ceresnik@gmail.com>
2018-08-08 22:54:04 +0200

    DegreesToRadiansConvertor refactoring II (all test green).

--- KGood/Source/DegreesToRadians/AngleInRadians.cs
+++ KGood/Source/DegreesToRadians/AngleInRadians.cs
@@ -5,6 +5,7 @@ namespace KGood.Source.DegreesToRadians
         private readonly int countOfQuarters;
         private readonly string piSign = "P";
         private readonly string fractionSeparator = "/";
+        private string multiplySign = "*";
         private string strNumerator;
         private string strDenominator;
 
@@ -17,19 +18,47 @@ namespace KGood.Source.DegreesToRadians
         {
             int numerator = countOfQuarters;
             int denominator = countOfQuarters > 0 ? 4 : 0;
+            if (HaveCommonDivisor(numerator, denominator))
+            {
+                int commonDivisor = FindCommonDivisor(numerator, denominator);
+                numerator = numerator / commonDivisor;
+                denominator = denominator / commonDivisor;
+            }
             ConvertNumeratorAndDenominatorToStringRepresentation(numerator, denominator);
-            return ComposeResult(numerator);
+            return ComposeResult(numerator, denominator);
+        }
+
+        private bool HaveCommonDivisor(int a, int b)
+        {
+            var biggestCommonDivisor = FindCommonDivisor(a, b);
+            return biggestCommonDivisor > 1;
+        }
+
+        private int FindCommonDivisor(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            return a == 0 ? b : a;
         }
 
         private void ConvertNumeratorAndDenominatorToStringRepresentation(int numerator, int denominator)
         {
-            strNumerator = countOfQuarters > 1 ? numerator.ToString() : "";
+            strNumerator = numerator > 1 ? numerator.ToString() : "";
             strDenominator = denominator > 1 ? denominator.ToString() : "";
         }
 
-        private string ComposeResult(int numerator)
+        private string ComposeResult(int numerator, int denominator)
         {
             string result = strNumerator;
+            if (numerator > 1)
+            {
+                result += multiplySign;
+            }
             if (numerator > 0)
             {
                 result += piSign;
@@ -38,7 +67,7 @@ namespace KGood.Source.DegreesToRadians
             {
                 result = "0";
             }
-            if (string.IsNullOrEmpty(strDenominator) == false)
+            if (denominator > 1)
             {
                 result += fractionSeparator;
             }
--- KGood/Source/DegreesToRadians/QuarterCounter.cs
+++ KGood/Source/DegreesToRadians/QuarterCounter.cs
@@ -4,6 +4,14 @@ namespace KGood.Source.DegreesToRadians
     {
         public int GetCountOfQuarters(int angleInDegrees)
         {
+            while (angleInDegrees < 0)
+            {
+                angleInDegrees += 360;
+            }
+            while (angleInDegrees > 360)
+            {
+                angleInDegrees -= 360;
+            }
             return angleInDegrees / 45;
         }
     }
