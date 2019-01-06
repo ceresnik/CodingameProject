1f02382ef12a8f5580c906e31f1d4fab9f3da736
Michal Ceresnik <michal.ceresnik@gmail.com>
2019-01-06 01:09:13 +0100

    Add StringFormatter

---
+++ CodingameProject/Source/AlphabetUtilities/StringFormatter.cs
@@ -0,0 +1,34 @@
+﻿namespace CodingameProject.Source.AlphabetUtilities
+{
+    public class StringFormatter
+    {
+        private readonly string format;
+
+        public StringFormatter(string format)
+        {
+            this.format = format;
+        }
+
+        public string Format(string input)
+        {
+            string result = "";
+            int currentInputIndex = 0;
+            foreach (char currentFormatChar in format)
+            {
+                if (currentFormatChar == 'X' || currentFormatChar == 'x')
+                {
+                    if (currentFormatChar == 'X')
+                        result += char.ToUpper(input[currentInputIndex]);
+                    else
+                        result += char.ToLower(input[currentInputIndex]);
+                    currentInputIndex++;
+                }
+                else
+                {
+                    result += currentFormatChar;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CodingameProject/Tests/AlphabetUtilities/StringFormatterTests.cs
@@ -0,0 +1,16 @@
+﻿using CodingameProject.Source.AlphabetUtilities;
+using NUnit.Framework;
+
+namespace CodingameProject.Tests.AlphabetUtilities
+{
+    public class StringFormatterTests
+    {
+        [Test]
+        public void Test()
+        {
+            var sut = new StringFormatter("Xx xxxx xx Xxxxxx!");
+            var result = sut.Format("mynaMeismICHAL");
+            Assert.That(result, Is.EqualTo("My name is Michal!"));
+        }
+    }
+}
