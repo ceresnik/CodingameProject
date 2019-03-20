cd780576a4d642bebf428cef9cac6c5a8a1f80d5
Michal Ceresnik <michal.ceresnik@gmail.com>
2019-03-20 16:21:45 +0100

    PresidentElection added

---
+++ CodingameProject/Source/PresidentElection/PresidentElectionEvaluation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CodingameProject.Source.PresidentElection
+{
+    public class PresidentElectionEvaluation
+    {
+        void Evaluate()
+        {
+            
+        }
+
+        void ReadoutElectionResults()
+        {
+            string jsonInputFile = "ElectionResults.json";
+            using (StreamReader r = new StreamReader(jsonInputFile))
+            {
+                var input = r.ReadToEnd();
+                var electionResults = JsonConvert.DeserializeObject<List<ElectionResults>>(input);
+                foreach (var result in electionResults)
+                {
+                    Console.WriteLine($"{result.name} : {result.result}");
+                }
+            }
+        }
+
+        class ElectionResults
+        {
+            public string name;
+            public double result;
+        }
+    }
+}
