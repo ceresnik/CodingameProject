22e2e6ae89c9e35fe2230011e936492e8c6f08cc
Michal <michal.ceresnik@gmail.com>
2019-04-06 22:26:24 +0200

    ProvidedTip has properties to get candidate name and his election gain.

--- CodingameProject/Source/PresidentElection/ProvidedTip.cs
+++ CodingameProject/Source/PresidentElection/ProvidedTip.cs
@@ -5,7 +5,11 @@
         private readonly CandidateNameElectionGainPairs tips;
         public readonly string TipperName;
         public string CandidateOnFirstPosition => tips[0].CandidateName;
+        public string CandidateOnSecondPosition => tips[1].CandidateName;
+        public string CandidateOnThirdPosition => tips[2].CandidateName;
         public double CandidateOnFirstPositionPercent => tips[0].ElectionGainInPercent;
+        public double CandidateOnSecondPositionPercent => tips[1].ElectionGainInPercent;
+        public double CandidateOnThirdPositionPercent => tips[2].ElectionGainInPercent;
 
         public ProvidedTip(string tipperName, CandidateNameElectionGainPairs tips)
         {
--- CodingameProject/Tests/PresidentElection/ProvidedTipTests.cs
+++ CodingameProject/Tests/PresidentElection/ProvidedTipTests.cs
@@ -12,14 +12,17 @@ namespace CodingameProject.Tests.PresidentElection
             //prepare
             var tipperName = "TipperName1";
             var candidateOnFirstPosition = "name1";
-            var candidateOnFirstPositionPercent = 11;
+            var candidateOnSecondPosition = "name2";
+            var candidateOnThirdPosition = "name3";
+            var firstPositionPercent = 11;
+            var secondPositionPercent = 12;
+            var thirdPositionPercent = 13;
 
             //act
-            CandidateNameElectionGainPairs tips = new CandidateNameElectionGainPairs();
-            CandidateNameElectionGainPair theFirstPlaceTip = new CandidateNameElectionGainPair(
-                candidateOnFirstPosition, candidateOnFirstPositionPercent);
-            CandidateNameElectionGainPair theSecondPlaceTip = new CandidateNameElectionGainPair("name2", 12);
-            CandidateNameElectionGainPair theThirdPlaceTip = new CandidateNameElectionGainPair("name3", 13);
+            var tips = new CandidateNameElectionGainPairs();
+            var theFirstPlaceTip = new CandidateNameElectionGainPair(candidateOnFirstPosition, firstPositionPercent);
+            var theSecondPlaceTip = new CandidateNameElectionGainPair(candidateOnSecondPosition, secondPositionPercent);
+            var theThirdPlaceTip = new CandidateNameElectionGainPair(candidateOnThirdPosition, thirdPositionPercent);
             tips.Add(theFirstPlaceTip);
             tips.Add(theSecondPlaceTip);
             tips.Add(theThirdPlaceTip);
@@ -29,8 +32,16 @@ namespace CodingameProject.Tests.PresidentElection
             Assert.That(sut.TipperName, Is.EqualTo(tipperName), "Tipper name is not as expected.");
             Assert.That(sut.CandidateOnFirstPosition, Is.EqualTo(candidateOnFirstPosition), 
                 "Candidate on first position not as expected.");
-            Assert.That(sut.CandidateOnFirstPositionPercent, Is.EqualTo(candidateOnFirstPositionPercent),
+            Assert.That(sut.CandidateOnSecondPosition, Is.EqualTo(candidateOnSecondPosition),
+                "Candidate on second position not as expected.");
+            Assert.That(sut.CandidateOnThirdPosition, Is.EqualTo(candidateOnThirdPosition),
+                "Candidate on third position not as expected.");
+            Assert.That(sut.CandidateOnFirstPositionPercent, Is.EqualTo(firstPositionPercent),
                 "Candidate on first position percent not as expected.");
+            Assert.That(sut.CandidateOnSecondPositionPercent, Is.EqualTo(secondPositionPercent),
+                "Candidate on second position percent not as expected.");
+            Assert.That(sut.CandidateOnThirdPositionPercent, Is.EqualTo(thirdPositionPercent),
+                "Candidate on third position percent not as expected.");
         }
     }
 }
