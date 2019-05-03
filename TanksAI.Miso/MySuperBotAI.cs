aee6dafc27b2c41bbe18ca97b1a6b0f2822d0e03
Michal Ceresnik <michal.ceresnik@gmail.com>
2019-05-03 12:44:05 +0200

    BotAI replaces TankAI.

---
+++ TanksAI.Miso/MySuperBotAI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingArena.AI;
+
+namespace TanksAI.Miso
+{
+    public class MySuperBotAI : IBotAI
+    {
+        public ITurnAction Update(IBot ownBot, IBattlefield battlefield)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDamaged(double damage, IBot shooter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnMoved(double distance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCollisionWith(IBot bot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResourcePicked()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnWeaponPicked(IWeapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnRegenerated()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BotName { get; }
+    }
+}
