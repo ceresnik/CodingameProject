3ad612a697a046cb2f3c3810c61e10292402e5ca
Michal Ceresnik <michal.ceresnik@gmail.com>
2019-10-12 23:44:10 +0200

    Add UnaryEncoder

---
+++ CodingameProject/Source/MathematicalUtilities/UnaryEncoder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CodingameProject.Source.MathematicalUtilities
+{
+    public class UnaryEncoder
+    {
+        private string serieOneSign = "0";
+        private string serieZeroSign = "00";
+        private string spaceSign = " ";
+
+        public string Encrypt(string input)
+        {
+            char inputChar = input[0];
+            string result = "";
+            int asciiValueDecimal = inputChar;
+            string asciiValueBinary = Convert.ToString(asciiValueDecimal, 2);
+
+            int lengthOfSerieCounter = 1;
+            int currentSerieSign = asciiValueBinary[0];
+            int index = 1;
+            int counter = 0;
+
+            while (index <= 6)
+            {
+                do
+                {
+                    counter++;
+                } while (currentSerieSign == asciiValueBinary[index++] && index < 6);
+                if (currentSerieSign == '1')
+                {
+                    result += serieOneSign;
+                }
+                else
+                {
+                    result += serieZeroSign;
+                }
+
+                result += spaceSign;
+
+                for (int i = 0; i < counter; i++)
+                {
+                    result += 0;
+                }
+
+                counter = 0;
+                currentSerieSign = asciiValueBinary[index];
+                result += spaceSign;
+            }
+            return result;
+        }
+    }
+}
---
+++ CodingameProject/Tests/MathematicalUtilities/UnaryEncoderTests.cs
@@ -0,0 +1,19 @@
+﻿using CodingameProject.Source.MathematicalUtilities;
+using NUnit.Framework;
+
+namespace CodingameProject.Tests.MathematicalUtilities
+{
+    [TestFixture]
+    public class UnaryEncoderTests
+    {
+        [Test]
+        public void Test_Encode_LetterC()
+        {
+            var sut = new UnaryEncoder();
+            string input = "C";
+            var result = sut.Encrypt(input);
+            var expected = "0 0 00 0000 0 00";
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
