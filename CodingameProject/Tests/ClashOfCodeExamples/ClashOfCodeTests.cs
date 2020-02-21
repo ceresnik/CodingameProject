ef97b144c2ee339b65ec5f0fd34db6651ee74261
Michal Ceresnik <michal.ceresnik@gmail.com>
2020-02-21 16:37:22 +0100

    Add ClashOfCodeTests.

---
+++ CodingameProject/Tests/ClashOfCodeExamples/ClashOfCodeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+
+namespace CodingameProject.Tests.ClashOfCodeExamples
+{
+    [TestFixture]
+    class ClashOfCodeTests
+    {
+
+        [Test]
+        public void SortItemsInListAccordingToLength()
+        {
+            var inputs = new List<string> { "abc", "xs", "longWord", "z" };
+            var sorted = inputs.OrderBy(x => x.Length).ToList();
+            sorted.ForEach(Console.WriteLine);
+        }
+    }
+}
