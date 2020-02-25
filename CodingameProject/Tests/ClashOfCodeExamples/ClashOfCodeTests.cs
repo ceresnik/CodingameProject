af01c347d65db9b394bac92f76b4b9b9596403e1
Michal Ceresnik <michal.ceresnik@gmail.com>
2020-02-25 10:16:16 +0100

    Add RemoveSpecialCharacters test.

--- CodingameProject/Tests/ClashOfCodeExamples/ClashOfCodeTests.cs
+++ CodingameProject/Tests/ClashOfCodeExamples/ClashOfCodeTests.cs
@@ -18,5 +18,15 @@ namespace CodingameProject.Tests.ClashOfCodeExamples
             var sorted = inputs.OrderBy(x => x.Length).ToList();
             sorted.ForEach(Console.WriteLine);
         }
+
+        [TestCase("1 309", "1309")]
+        [TestCase("1,309", "1309")]
+        [TestCase("1.309", "1309")]
+        [Test]
+        public void RemoveSpecialCharacters(string input, string expected)
+        {
+            string result = new string(input.Where(x => x >= '0' && x <= '9').ToArray());
+            Assert.That(result, Is.EqualTo(expected));
+        }
     }
 }
