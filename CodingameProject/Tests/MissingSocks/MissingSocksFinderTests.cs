7237df43ba1d37108d17741571b92b6bd40f73b9
Michal Ceresnik <michal.ceresnik@gmail.com>
2020-10-08 00:07:58 +0200

    Moved MissingSocksFinder class to own file.

--- CodingameProject/Tests/MissingSocks/MissingSocksFinderTests.cs
+++ CodingameProject/Tests/MissingSocks/MissingSocksFinderTests.cs
@@ -1,44 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
+using CodingameProject.Source.MissingSocks;
 
 using NUnit.Framework;
 
 
 namespace CodingameProject.Tests.MissingSocks
 {
-    /*
-    Often (I hope) you wash your clothes. And sometimes a sock disappears.
-    Now you are in front of your washing machine and you empty it. While emptying you want to know if you have any lost socks and which ones they are.
-    Input
-    Line 1: n the number of clothes and underwear in the washing machine.
-    Next n lines: clothes type clothes size clothes color a string, an int, and a string.
-
-    Socks have the sock clothes type.
-    Pants have the pants clothes type.
-    T-Shirts have the t-shirt clothes type.
-    And so on...
-
-    Two socks form a pair if they are if they have the same clothes type, clothes size, and clothes color.
-
-    Any sock coming out of the washing machine that is not part of pair indicates that you have lost that other sock.
-    Output
-    First line: The number of disappeared socks.
-    Then: The sock size and the sock color (separated by a space) of all missing socks separated by a new line. Nothing if all socks are back from the washing.
-    Socks must be in the numerical then alphabetical order. That means socks must be sorted by their size and if they have the same size by their colors.
-    Constraints
-    0 ≤ n < 10
-    Example
-    Input
-    5
-    t-shirt 2 blue
-    sock 42 red
-    sock 42 red
-    short 7 white
-    sock 37 grey
-    Output:  1 37 grey
-    */
-
     /*
      Acceptance tests:
      1)7
@@ -127,7 +96,7 @@ namespace CodingameProject.Tests.MissingSocks
         [Test, TestCaseSource(nameof(GetTest7Input))]
         public void Test7(string exp, IEnumerable<(string, int, string)> inputClothes)
         {
-            var socksWithoutPair = FindSocksWithoutPair(inputClothes);
+            var socksWithoutPair = MissingSocksFinder.FindSocksWithoutPair(inputClothes);
             Console.WriteLine(socksWithoutPair.Count);
             foreach (var sockWithoutPair in socksWithoutPair)
             {
@@ -135,22 +104,6 @@ namespace CodingameProject.Tests.MissingSocks
                 Console.WriteLine(output);
                 Assert.That(output, Is.EqualTo(exp));
             }
-        }
-
-        private static List<(string, int, string)> FindSocksWithoutPair(IEnumerable<(string, int, string)> input)
-        {
-            var socks = input.Where(item => item.Item1.Equals("sock")).ToList();
-
-            var socksWithoutPair = new List<(string, int, string)>();
-            foreach (var sock in socks)
-            {
-                if (socks.Count(x => x.Item2 == sock.Item2 && x.Item3 == sock.Item3) == 1)
-                {
-                    socksWithoutPair.Add(sock);
-                }
-            }
-
-            return socksWithoutPair;
         }
     }
 }
