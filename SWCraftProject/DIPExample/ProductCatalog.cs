caf6fa77d89ab03cd6904122c4de8e1607958974
Michal Ceresnik - md3iv35c <michal.ceresnik@gmail.com>
2022-04-21 09:28:11 +0200

    Added DependencyInversion example ProductCatalog class and tests.

---
+++ SWCraftProject/DIPExample/ProductCatalog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SWCraftProject.DIPExample
+{
+    class ProductCatalog
+    {
+        public void ListAllProducts()
+        {
+            var sqlProductRepository = new SqlProductRepository();
+            IList<string> productNames = sqlProductRepository.GetAllProductNames();
+            Console.WriteLine(string.Join(", ", productNames));
+        }
+    }
+
+    internal class SqlProductRepository
+    {
+        public IList<string> GetAllProductNames()
+        {
+            return new List<string>{"soap", "toothpaste", "shampoo"};
+        }
+    }
+
+    public class ProductCatalogTests
+    {
+        [Test]
+        public void Test()
+        {
+            var productCatalog = new ProductCatalog();
+            productCatalog.ListAllProducts();
+        }
+    }
+}
---
+++ SWCraftProject/DIPExample/good/ProductCatalog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+
+namespace SWCraftProject.DIPExample.good
+{
+    class ProductCatalog
+    {
+        private readonly IProductRepository myProductRepository;
+
+        public ProductCatalog(IProductRepository productRepository)
+        {
+            myProductRepository = productRepository;
+        }
+
+        public void ListAllProducts()
+        {
+            IList<string> productNames = myProductRepository.GetAllProductNames();
+            Console.WriteLine(string.Join(", ", productNames));
+        }
+    }
+
+    public interface IProductRepository
+    {
+        IList<string> GetAllProductNames();
+    }
+
+    public class ProductCatalogTests
+    {
+        [Test]
+        public void Test()
+        {
+            var mockProductRepository = new Mock<IProductRepository>();
+            IList<string> testProductNames = new List<string>
+            {
+                "testProduct1", "testProduct2"
+            };
+            mockProductRepository.Setup(x => x.GetAllProductNames()).Returns(testProductNames);
+            var productCatalog = new ProductCatalog(mockProductRepository.Object);
+            productCatalog.ListAllProducts();
+        }
+    }
+
+}
