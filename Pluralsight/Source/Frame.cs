0a0bf46be8069e2c039fabc6fa3217e1c56d1e3d
Michal Ceresnik - md3iv35c <michal.ceresnik@siemens-healthineers.com>
2023-02-10 17:07:03 +0100

    Renamed to Frame. Added Width property.

---
+++ Pluralsight/Source/Frame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pluralsight.Source
+{
+    internal class Frame
+    {
+        private int length;
+        private int width;
+
+        public int Length
+        {
+            get => length;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length must be positive.");
+                }
+                length = value;
+            }
+            //but remember - letting an object to have it's dimension modifiable is very dangerous idea.
+        }
+
+        public int Width
+        {
+            get => width;
+            set {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width must be positive.");
+                }
+                width = value;
+            }
+        }
+    }
+}
---
+++ Pluralsight/Tests/FrameTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using Pluralsight.Source;
+
+namespace Pluralsight.Tests
+{
+    [TestFixture]
+    class FrameTests
+    {
+        [Test]
+        public void Test_PositiveLength()
+        {
+            var frame = new Frame
+            {
+                Length = 10
+            };
+            Assert.That(frame.Length, Is.EqualTo(10), "Lenght of frame not as expected.");
+        }
+
+        [Test]
+        public void Test_PositiveWidth()
+        {
+            var frame = new Frame
+            {
+                Width = 20
+            };
+            Assert.That(frame.Width, Is.EqualTo(20), "Width of frame not as expected.");
+        }
+
+        [Test]
+        public void Test_NegativeLength_ThrowsException()
+        {
+            var frame = new Frame();
+            Assert.Throws<ArgumentException>(() => frame.Length = -1);
+        }
+
+        [Test]
+        public void Test_NegativeWidth_ThrowsException()
+        {
+            var frame = new Frame();
+            Assert.Throws<ArgumentException>(() => frame.Width = -1);
+        }
+
+        [Test]
+        public void Test_ZeroLength_ThrowsException()
+        {
+            var frame = new Frame();
+            Assert.Throws<ArgumentException>(() => frame.Length = 0);
+        }
+
+        [Test]
+        public void Test_ZeroWidth_ThrowsException()
+        {
+            var frame = new Frame();
+            Assert.Throws<ArgumentException>(() => frame.Width = 0);
+        }
+    }
+}
