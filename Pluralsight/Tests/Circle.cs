0fe3823113350ddfd8ed8839fbe72a3617be32ce
Michal Ceresnik - md3iv35c <michal.ceresnik@siemens-healthineers.com>
2023-02-10 22:28:32 +0100

    Introduced Circle class.

---
+++ Pluralsight/Tests/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace Pluralsight.Tests
+{
+    internal class Circle
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Radius must not be negative.");
+                }
+                radius = value;
+            }
+        }
+
+        public Point Centre { get; set; }
+    }
+}
---
+++ Pluralsight/Tests/CircleTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+
+namespace Pluralsight.Tests
+{
+    class CircleTests
+    {
+        [Test]
+        public void Circle_NegativeRadius_ThrowsException()
+        {
+            var circle = new Circle();
+            Assert.Throws<ArgumentException>(() => circle.Radius = -1.2);
+        }
+
+        [Test]
+        public void Circle_ZeroRadius_ThrowsException()
+        {
+            var circle = new Circle();
+            Assert.Throws<ArgumentException>(() => circle.Radius = 0);
+        }
+
+        [Test]
+        public void Circle_HasCentre()
+        {
+            var circle = new Circle();
+            var coordinatesOfCentre = new Point(2, 5);
+            circle.Centre = coordinatesOfCentre;
+            Assert.That(circle.Centre, Is.EqualTo(coordinatesOfCentre), "Coordinates of Centre of the circle not as expected.");
+        }
+    }
+}
