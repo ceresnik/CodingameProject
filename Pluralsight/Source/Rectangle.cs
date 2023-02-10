aff760af809427e2424552dc7bc7d531c2af17ce
Michal Ceresnik - md3iv35c <michal.ceresnik@siemens-healthineers.com>
2023-02-10 16:53:27 +0100

    Introduced Rectangle class in Pluralsight project.

---
+++ Pluralsight/Source/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pluralsight.Source
+{
+    internal class Rectangle
+    {
+        private int length;
+
+        public int Length
+        {
+            get => length;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length must be positive.");
+                }
+                length = value;
+            }
+        }
+    }
+}
---
+++ Pluralsight/Tests/RectangleTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Pluralsight.Source;
+
+namespace Pluralsight.Tests
+{
+    [TestFixture]
+    class RectangleTests
+    {
+        [Test]
+        public void Test_PositiveLength()
+        {
+            var rectangle = new Rectangle();
+            rectangle.Length = 10;
+            Assert.That(rectangle.Length, Is.EqualTo(10), "Lenght of rectangle not as expected.");
+        }
+
+        [Test]
+        public void Test_NegativeLength_ThrowsException()
+        {
+            IList<int> a = new List<int>();
+            a.Add(2);
+            var b = a.Distinct();
+            var rectangle = new Rectangle();
+            Assert.Throws<ArgumentException>(() => rectangle.Length = -1);
+        }
+
+        [Test]
+        public void Test_ZeroLength_ThrowsException()
+        {
+            var rectangle = new Rectangle();
+            Assert.Throws<ArgumentException>(() => rectangle.Length = 0);
+        }
+    }
+}
