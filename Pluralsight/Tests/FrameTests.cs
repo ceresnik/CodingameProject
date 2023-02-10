8cf59a68e73062c3ff5ae72cfcd3a04add78aedd
Michal Ceresnik - md3iv35c <michal.ceresnik@siemens-healthineers.com>
2023-02-10 22:00:04 +0100

    Used different numbers in TC for Lenth and Width. Changed order of test methods.

--- Pluralsight/Tests/FrameTests.cs
+++ Pluralsight/Tests/FrameTests.cs
@@ -8,13 +8,31 @@ namespace Pluralsight.Tests
     class FrameTests
     {
         [Test]
-        public void Test_PositiveLength()
+        public void Test_ZeroLength_ThrowsException()
         {
-            var frame = new Frame
-            {
-                Length = 10
-            };
-            Assert.That(frame.Length, Is.EqualTo(10), "Length of frame not as expected.");
+            var frame = new Frame();
+            Assert.Throws<ArgumentException>(() => frame.Length = 0);
+        }
+
+        [Test]
+        public void Test_ZeroWidth_ThrowsException()
+        {
+            var frame = new Frame();
+            Assert.Throws<ArgumentException>(() => frame.Width = 0);
+        }
+
+        [Test]
+        public void Test_NegativeLength_ThrowsException()
+        {
+            var frame = new Frame();
+            Assert.Throws<ArgumentException>(() => frame.Length = -11.9);
+        }
+
+        [Test]
+        public void Test_NegativeWidth_ThrowsException()
+        {
+            var frame = new Frame();
+            Assert.Throws<ArgumentException>(() => frame.Width = -1.1);
         }
 
         [Test]
@@ -38,41 +56,23 @@ namespace Pluralsight.Tests
         }
 
         [Test]
+        public void Test_PositiveLength()
+        {
+            var frame = new Frame
+            {
+                Length = 10.4
+            };
+            Assert.That(frame.Length, Is.EqualTo(10.4), "Length of frame not as expected.");
+        }
+
+        [Test]
         public void Test_PositiveWidth()
         {
             var frame = new Frame
             {
-                Width = 20
+                Width = 20.7
             };
-            Assert.That(frame.Width, Is.EqualTo(20), "Width of frame not as expected.");
-        }
-
-        [Test]
-        public void Test_NegativeLength_ThrowsException()
-        {
-            var frame = new Frame();
-            Assert.Throws<ArgumentException>(() => frame.Length = -1);
-        }
-
-        [Test]
-        public void Test_NegativeWidth_ThrowsException()
-        {
-            var frame = new Frame();
-            Assert.Throws<ArgumentException>(() => frame.Width = -1);
-        }
-
-        [Test]
-        public void Test_ZeroLength_ThrowsException()
-        {
-            var frame = new Frame();
-            Assert.Throws<ArgumentException>(() => frame.Length = 0);
-        }
-
-        [Test]
-        public void Test_ZeroWidth_ThrowsException()
-        {
-            var frame = new Frame();
-            Assert.Throws<ArgumentException>(() => frame.Width = 0);
+            Assert.That(frame.Width, Is.EqualTo(20.7), "Width of frame not as expected.");
         }
     }
 }
